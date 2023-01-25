6eec4080b88bd62bc982043cd2b06af7d5ff55fd
SerhiiKutovyi <wagenrex@gmail.com>
2023-01-25 13:57:13 +0200

    name

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,3 +1,9 @@
+// import styled from 'styled-components';
+// import PropTypes from 'prop-types';
+
+const API_KEY = '32386885-8dbf1bc36075d10a6eaf5580b';
+//https://pixabay.com/api/?q=cat&page=1&key=your_key&image_type=photo&orientation=horizontal&per_page=12
+
 export const App = () => {
   return (
     <div
@@ -7,7 +13,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
